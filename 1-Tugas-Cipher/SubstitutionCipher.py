2df27f3f1528a002e8c5a479990a91e989c466bc
shehaannisa <103401710+shehaannisa@users.noreply.github.com>
2023-03-21 20:22:51 +0700

    Add files via upload

---
+++ 1-Tugas-Cipher/SubstitutionCipher.py
@@ -0,0 +1,88 @@
+import tkinter as tk
+from tkinter import ttk
+import string
+
+# Membuat dictionary huruf yang diganti
+substitution_dict = {
+    'a': 'q',
+    'b': 'w',
+    'c': 'e',
+    'd': 'r',
+    'e': 't',
+    'f': 'y',
+    'g': 'u',
+    'h': 'i',
+    'i': 'o',
+    'j': 'p',
+    'k': 'a',
+    'l': 's',
+    'm': 'd',
+    'n': 'f',
+    'o': 'g',
+    'p': 'h',
+    'q': 'j',
+    'r': 'k',
+    's': 'l',
+    't': 'z',
+    'u': 'x',
+    'v': 'c',
+    'w': 'v',
+    'x': 'b',
+    'y': 'n',
+    'z': 'm'
+}
+
+# Membuat fungsi untuk melakukan penggantian huruf
+def substitution_cipher(text):
+    result = ""
+    for char in text.lower():
+        if char in string.ascii_lowercase:
+            result += substitution_dict[char]
+        else:
+            result += char
+    return result
+
+# Membuat fungsi untuk button "Enkripsi"
+def enkripsi():
+    plaintext = plaintext_entry.get()
+    ciphertext = substitution_cipher(plaintext)
+    ciphertext_entry.delete(0, tk.END)
+    ciphertext_entry.insert(0, ciphertext)
+
+# Membuat fungsi untuk button "Dekripsi"
+def dekripsi():
+    ciphertext = ciphertext_entry.get()
+    plaintext = substitution_cipher(ciphertext)
+    plaintext_entry.delete(0, tk.END)
+    plaintext_entry.insert(0, plaintext)
+
+# Membuat window
+root = tk.Tk()
+root.title("Substitution Cipher")
+
+# Membuat label "Plaintext"
+plaintext_label = ttk.Label(root, text="Plaintext:")
+plaintext_label.grid(column=0, row=0, padx=5, pady=5)
+
+# Membuat entry untuk plaintext
+plaintext_entry = ttk.Entry(root, width=30)
+plaintext_entry.grid(column=1, row=0, padx=5, pady=5)
+
+# Membuat label "Ciphertext"
+ciphertext_label = ttk.Label(root, text="Ciphertext:")
+ciphertext_label.grid(column=0, row=1, padx=5, pady=5)
+
+# Membuat entry untuk ciphertext
+ciphertext_entry = ttk.Entry(root, width=30)
+ciphertext_entry.grid(column=1, row=1, padx=5, pady=5)
+
+# Membuat button "Enkripsi"
+enkripsi_button = ttk.Button(root, text="Enkripsi", command=enkripsi)
+enkripsi_button.grid(column=0, row=2, padx=5, pady=5)
+
+# Membuat button "Dekripsi"
+dekripsi_button = ttk.Button(root, text="Dekripsi", command=dekripsi)
+dekripsi_button.grid(column=1, row=2, padx=5, pady=5)
+
+# Menjalankan window
+root.mainloop()
